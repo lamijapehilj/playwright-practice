8143d35c98a783357eeef679351e9f8c0048bbf9
Lamija <lamija@MacBook-Pro-3.local>
2026-01-16 18:28:54 +0100

    Restore login changes after hotfix

--- tests/login.tests.js
+++ tests/login.tests.js
@@ -2,4 +2,21 @@ function sabiranje(a , b) {
 return a + b
 };
 
-console.log(sabiranje(3, 2));
+console.log(sabiranje(35, 2));
+
+
+
+function returnNumBiggerThanTwo(arr) {
+
+    for(var i = 0; i < arr.length; i++) {
+    
+        if(arr[i] > 2) {
+            return arr[i];
+        }
+       
+    }
+return false
+}
+
+console.log(returnNumBiggerThanTwo([1, 2, 5, 7]) )// 5
+console.log(returnNumBiggerThanTwo([0, 1, 2]) )   // false
